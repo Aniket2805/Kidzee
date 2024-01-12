8bbf98c04b5228e78ec35770dd9b510190856d3f
Aniket2805 <aniketrocks2468@gmail.com>
2024-01-12 13:28:58 +0530

    w

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Cache the navigation element and its initial offset
 var nav = document.getElementById("nav");
+var navwh = document.getElementsByClassName("white");
 var navLinks = document.querySelectorAll(".nav-link");
 var sticky = document.getElementById("sticky");
 var stickyHeaderTop = nav.offsetTop;
@@ -9,15 +10,12 @@ function handleScroll() {
   // Check if the scroll position is below the sticky header
   if (window.scrollY > stickyHeaderTop) {
     // Apply styles for sticky header
+    nav.classList.remove("white");
     nav.style.position = "fixed";
     nav.style.top = "0px";
     nav.style.zIndex = "999";
     nav.style.backgroundColor = "#65318e";
-    nav.style.color = "white";
-
-    // Toggle class for navigation links
     navLinks.forEach(function (link) {
-      link.classList.remove(".white");
       link.style.color = "white";
     });
     // Display the sticky element
@@ -26,14 +24,9 @@ function handleScroll() {
     // Apply styles for non-sticky header
     nav.style.position = "static";
     nav.style.backgroundColor = "white";
-    nav.style.color = "black";
-
-    // Remove class and set color for navigation links
     navLinks.forEach(function (link) {
-      link.classList.remove(".white");
       link.style.color = "black";
     });
-
     // Hide the sticky element
     sticky.style.display = "none";
   }
